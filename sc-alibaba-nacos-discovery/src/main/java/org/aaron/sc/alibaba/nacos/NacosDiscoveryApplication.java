1c4294558a99bb8a48e5d85e7d866ccab5da22fb
Menng <concisesky@gmail.com>
2019-01-25 20:08:58 +0800

    nacos consumber

--- sc-alibaba-nacos-discovery/src/main/java/org/aaron/sc/alibaba/nacos/NacosDiscoveryApplication.java
+++ sc-alibaba-nacos-discovery/src/main/java/org/aaron/sc/alibaba/nacos/NacosDiscoveryApplication.java
@@ -3,6 +3,9 @@ package org.aaron.sc.alibaba.nacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -11,6 +14,13 @@ public class NacosDiscoveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosDiscoveryApplication.class, args);
     }
-
 }
 
+@RestController
+class TestController {
+
+    @GetMapping("/echo/{hello}")
+    public String echo(@PathVariable String hello) {
+        return "Hello Springcloud Alibaba";
+    }
+}
