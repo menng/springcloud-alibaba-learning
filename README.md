f5f4970857443f8b6da61e025bf60830e95fcdb3
Menng <concisesky@gmail.com>
2019-01-25 11:45:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+### springcloud-alibaba 示例项目
+此示例项目基于springcloud-alibaba提供的组件
+
+#### nacos 启动
+关于[nacos](https://github.com/alibaba/nacos)的启动，官方文档有介绍，这里不再赘述。  
+nacos启动地址：http://127.0.0.1:8848/nacos/  
+username/password: nacos/nacos
+
---
+++ sc-alibaba-nacos-discovery/src/main/java/org/aaron/sc/alibaba/nacos/NacosDiscoveryApplication.java
@@ -0,0 +1,16 @@
+package org.aaron.sc.alibaba.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDiscoveryApplication.class, args);
+    }
+
+}
+
